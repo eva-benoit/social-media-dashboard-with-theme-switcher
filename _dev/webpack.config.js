4dee807ffe5d63ecb11871be51a993896cee2145
Eva Benoit <dev@web-be.fr>
2020-04-25 00:51:52 +0200

    Build HTML primary elements and create main SASS files

--- _dev/webpack.config.js
+++ _dev/webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var DelayPlugin = require("webpack-delay-plugin");
 
 module.exports = {
   entry: {
@@ -23,13 +24,28 @@ module.exports = {
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          },
+          'sass-loader',
+          'postcss-loader'
+        ]
       }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'theme.css'
+    }),
+    new DelayPlugin({
+      delay: 1000,
+      lifecycleEvents: ['run', 'watch-run'],
+      verbose: false
     })
   ]
 };
