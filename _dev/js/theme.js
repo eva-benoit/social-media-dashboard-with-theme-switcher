e1966a80933643438820e65b7b440022b6c14271
Eva Benoit <dev@web-be.fr>
2020-04-28 13:05:34 +0200

    Add IE11 compatibility

--- _dev/js/theme.js
+++ _dev/js/theme.js
@@ -2,6 +2,13 @@ import $ from "jquery";
 
 $(function() {
 
+  // Detect IE browser
+  var ua = navigator.userAgent;
+  if (ua.indexOf('MSIE ') !== -1 || ua.indexOf('Trident/') !== -1) {
+    $('html').addClass('ie');
+  }
+
+  // Enable toggle button mecanisme
   $('.toggle-btn').click(function() {
     $('html').toggleClass('light-mode dark-mode');
     $(this).toggleClass('on');
